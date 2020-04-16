1238811e8b635d8a8343ec63e6fbe517608d72fc
KeithTMaxwell <sherlocklcc@gmail.com>
2020-04-16 23:31:48 +0800

    add more comments

--- UDPSocket.py
+++ UDPSocket.py
@@ -5,7 +5,10 @@ class UDPSocket:
     def __init__(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    # only server need to bind ip port
+    # only server needs to bind ip port
+    # input     ip: string, ipv4
+    #           port: int
+    # output    True/False: success/fail
     def bind_ip_port(self, ip, port):
         try:
             ip_port = (ip, port)
@@ -15,21 +18,28 @@ class UDPSocket:
             print('socket bind ip failed: ' + str(error))
             return False
 
-    # can be executed without binding ip port
+    # send a message to specified ip:port
+    # input     msg: string
+    #           ip: string, destination ip
+    #           port: int, destination port
+    # output    True/False: success/fail
     def send_to(self, msg, ip, port):
-        dist_ip_port = (ip, port)
+        ip_port = (ip, port)
         try:
-            self.socket.sendto(msg.encode(), dist_ip_port)
+            self.socket.sendto(msg.encode(), ip_port)
             return True
         except WindowsError as error:
             print('send message failed: ' + str(error))
             return False
 
-    # can be executed without binding ip port
-    # read receive buffer, an UDP package max_len is 8192
-    def receive(self, max_buffer_len=1024):
+    # read socket buffer
+    # input     read_bytes: read bytes from buffer
+    # output    message: string, received message
+    #           ip: string, source ip
+    #           port: int, source port
+    def receive(self, read_bytes=1024):
         try:
-            msg, (ip, port) = self.socket.recvfrom(max_buffer_len)
+            msg, (ip, port) = self.socket.recvfrom(read_bytes)
             return str(msg.decode()), ip, port
         except WindowsError as error:
             print('receiving error: ' + str(error))
